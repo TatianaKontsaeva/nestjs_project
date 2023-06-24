7b2a862e22007843e99e3af6c3e3cedb10e6fc7d
Tatiana <tanya_13.88@mail.ru>
2023-06-24 16:05:09 +0300

    Подключение ORM

--- src/category/entities/category.entity.ts
+++ src/category/entities/category.entity.ts
@@ -1,1 +1,11 @@
-export class Category {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn()
+    id: number;
+  
+    @Column()
+    categoryName: string;
+  
+}
