2bf612d254037f81cf51909a669f3fc26171fc55
Tatiana <tanya_13.88@mail.ru>
2023-07-06 10:04:11 +0300

    tests added

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,22 +15,48 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+  // //done
+  // it('/ (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/')
+  //     .expect(200)
+  //     .expect('Hello World!');
+  // });
 
-  it('/comments (GET)', () => {
-    return request(app.getHttpServer()).get('/comments').expect(200).expect([]);
-  }); 
+  // // //done
+  // it('/comments (GET)', () => {
+  //   return request(app.getHttpServer()).get('/comments').expect(200).expect([]);
+  // });
 
+  // //done
+  // it('/comments/1 (GET)', () => {
+  //   return request(app.getHttpServer())
+  //     .get('/comments/1')
+  //     .expect(200)
+  //     .expect('');
+  // });
 
+  // it('/comments (POST)', () => {
+  //   return request(app.getHttpServer())
+  //     .post('/comments')
+  //     .send({ text: 'New' })
+  //     .expect(201)
+  //     .expect((res) => {
+  //       expect(res.body).toEqual(
+  //         expect.objectContaining({
+  //           id: expect.any(Number),
+  //           text: expect.any(String),
+  //         }),
+  //       );
+  //     });
+  // });
 
+  // it('/tasks (GET)', () => {
+  //   return request(app.getHttpServer()).get('/tasks').expect(200).expect([]);
+  // });
 
   afterAll((done) => {
     app.close();
     done();
-  })
+  });
 });
